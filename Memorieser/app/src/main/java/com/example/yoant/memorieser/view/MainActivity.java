a0295cfd9fb58f4819c60018615eaf7bc4b235d4
antonson <tf.2.98@mail.ru>
2017-02-17 21:11:45 +0300

    Login Activity UI

--- Memorieser/app/src/main/java/com/example/yoant/memorieser/view/MainActivity.java
+++ Memorieser/app/src/main/java/com/example/yoant/memorieser/view/MainActivity.java
@@ -1,31 +1,38 @@
 package com.example.yoant.memorieser.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.yoant.memorieser.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button redirectButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        redirectButton = (Button)findViewById(R.id.tmpLoginButton);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "IF YOU SEE THIS, ALL WILL BE AWESOME IN YOUR LIFE", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void buttonClickFunction(View v) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
     }
 
 }
